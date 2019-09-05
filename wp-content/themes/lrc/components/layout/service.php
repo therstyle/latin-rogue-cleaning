78bc9b808924f5c47fee50a3b997c8c03b96c0cc
Chris Roberts <chris@829llc.com>
2019-09-05 11:20:47 -0400

    Updated service component

--- wp-content/themes/lrc/components/layout/service.php
+++ wp-content/themes/lrc/components/layout/service.php
@@ -1,8 +1,21 @@
 <?php
 $serviceHeadline = get_sub_field('service_headline');
 $serviceImage = get_sub_field('service_image');
+$serviceText = get_sub_field('service_text');
 ?>
 
 <article class="service">
-
+  <?php if($serviceHeadline || $serviceImage): ?>
+    <header>
+      <?php 
+      if($serviceImage) {
+        echo wp_get_attachment_image($serviceImage, 'full');
+      }
+      
+      elseif ($serviceHeadline) {
+        echo '<h3>'.$serviceHeadline.'</h3>';
+      }
+      ?> 
+    </header>
+  <?php endif; ?>
 </article>
