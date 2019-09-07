6db4420a787ab9a434049870a574d53b4661c1f4
therstyle <therstyle@gmail.com>
2019-09-07 11:43:47 -0400

    Updated footer styling + removed floats

--- wp-content/themes/lrc/footer.php
+++ wp-content/themes/lrc/footer.php
@@ -1,12 +1,10 @@
     <footer class="main">
       <div class="wrapper">
-        <div class="sig-wrap">
-          <div class="sig">
-            Web Design By
-            <a href="https://rstyledesign.com" target="_blank">
-              <img src="<?php echo get_template_directory_uri(); ?>/src/images/sig.png" alt="Web Design By Rstyle Design" />
-            </a>
-          </div>
+        <div class="sig">
+          Web Design By
+          <a href="https://rstyledesign.com" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/images/sig.png" alt="Web Design By Rstyle Design" />
+          </a>
         </div>
       </div>
     </footer>
