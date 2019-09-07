06771ae0ae4846491520bfee6d5c1dc1febd353a
therstyle <therstyle@gmail.com>
2019-09-07 11:00:59 -0400

    Replaced images with CSS gradients

--- wp-content/themes/lrc/components/layout/contact.php
+++ wp-content/themes/lrc/components/layout/contact.php
@@ -7,29 +7,31 @@ $contactForm = get_field('contact_form');
 
 <a class="scroll-anchor" id="quote"></a>
 <div class="bottom">
-	<section class="wrapper">
-    <article class="quote-left">
-      <?php if($contactHeadline): ?>
-        <header>
-          <?php echo $contactHeadline; ?>
-        </header>
-      <?php endif; ?>
-      
-      <?php 
-      if($contactText) {
-        echo $contactText;
-      }
-      ?>
-    </article>
+	<div class="dirt">
+    <section class="wrapper">
+      <article class="quote-left">
+        <?php if($contactHeadline): ?>
+          <header>
+            <?php echo $contactHeadline; ?>
+          </header>
+        <?php endif; ?>
+        
+        <?php 
+        if($contactText) {
+          echo $contactText;
+        }
+        ?>
+      </article>
 
-    <div class="quote-right">
-      <?php 
-      if($contactForm) {
-        echo $contactForm;
-      }
-      ?>
-    </div>
-  </section>
+      <div class="quote-right">
+        <?php 
+        if($contactForm) {
+          echo $contactForm;
+        }
+        ?>
+      </div>
+    </section>
 
-  <?php get_footer(); ?>
+    <?php get_footer(); ?>
+  </div>
 </div>
--- wp-content/themes/lrc/components/pages/home.php
+++ wp-content/themes/lrc/components/pages/home.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 
 <?php get_template_part('components/layout/hero'); ?>
-<?php get_template_part('components/layout/about'); ?>
-<?php get_template_part('components/layout/services'); ?>
+
+<div class="bottom-grass">
+  <?php get_template_part('components/layout/about'); ?>
+  <?php get_template_part('components/layout/services'); ?>
+</div>
+
 <?php get_template_part('components/layout/contact'); ?>
