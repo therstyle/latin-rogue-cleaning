fec11c53b9d756a9d4bcc87e0c2854f951ba94c0
Chris Roberts <therstyle@gmail.com>
2019-09-26 08:29:10 -0400

    Added JS source files

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,14 @@
+import SmoothScroll from '../smoothscroll';
+import Slider from '../slider';
+import Navigation from '../navigation';
+//import ScrollDirection from '../scrolldirection';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    SmoothScroll.init();
+    Slider.init();
+    Navigation.init();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ resources/assets/scripts/smoothscroll.js
@@ -0,0 +1,30 @@
+class SmoothScroll {
+  constructor() {
+    this.links = document.querySelectorAll('header.main .menu > li > a, .logo');
+  }
+
+  scroll(element) {
+    const selector = document.getElementById(element);
+    window.scrollTo({
+      behavior: 'smooth',
+      left: 0,
+      top: selector.offsetTop,
+    });
+    document.querySelector('body').classList.remove('menu-open');
+  }
+
+  init() {
+    const links = this.links;
+    links.forEach(element => {
+      element.addEventListener('click', (e) => {
+        e.preventDefault();
+        const source = element.getAttribute('href').replace('#', '');
+        if (document.getElementById(source)) {
+          this.scroll(source);
+        }
+      });
+    });
+  }
+}
+
+export default new SmoothScroll();
